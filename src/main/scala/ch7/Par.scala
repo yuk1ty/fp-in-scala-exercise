106904549620c8963a781692805544d9353edb6a
Yuki <yuki.mul.tiplus@gmail.com>
2018-01-09 07:58:03 +0900

    add parallelism exercises

---
+++ src/main/scala/ch7/Par.scala
@@ -0,0 +1,48 @@
+package ch7
+
+import java.util.concurrent._
+
+/*
+ * Copyright 2017 Yuki Toyoda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+object Par {
+
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+
+    override def isCancelled: Boolean = false
+
+    override def isDone: Boolean = true
+
+    override def get(timeout: Long, unit: TimeUnit): A = get
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] = es => es.submit(() => a(es).get)
+}
+
+trait Par[A]
