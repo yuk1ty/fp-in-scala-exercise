95ebde94b42b1558ae351998c36d19dbcba1e77e
Yuki <yuki.mul.tiplus@gmail.com>
2018-01-12 08:09:51 +0900

    Foldable

--- src/main/scala/ch10/Monoid.scala
+++ src/main/scala/ch10/Monoid.scala
@@ -88,15 +88,29 @@ object Monoid {
     override def zero: WC = Stub("")
   }
 
+  def dual[A](m: Monoid[A]): Monoid[A] = new Monoid[A] {
+    override def zero: A = m.zero
+    override def op(a1: A, a2: A): A = m.op(a1, a2)
+  }
+
   // def count
 
   trait Foldable[F[_]] {
-    def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B
-    def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B
+    def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B =
+      foldMap(as)(f.curried)(endoMonoid[B])(z)
+    def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B =
+      foldMap(as)(a => (b: B) => f(b, a))(dual(endoMonoid[B]))(z)
     def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
       foldRight(as)(mb.zero)((a, b) => mb.op(f(a), b))
-    def concatenate[A](as: F[A])(m: Monoid[A]): A
+    def concatenate[A](as: F[A])(m: Monoid[A]): A = foldLeft(as)(m.zero)(m.op)
   }
 
-  trait ListFoldable extends Foldable[List] {}
+  trait ListFoldable extends Foldable[List] {
+    override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
+      as.foldRight(z)(f)
+    override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
+      as.foldLeft(z)(f)
+    override def foldMap[A, B](as: List[A])(f: A => B)(mb: Monoid[B]): B =
+      foldLeft(as)(mb.zero)((b, a) => mb.op(b, f(a)))
+  }
 }
