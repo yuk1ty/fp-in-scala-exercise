ae17b83f835daff691161df9b41d30cf9b02d5c1
Yuki <yuki.mul.tiplus@gmail.com>
2018-01-14 12:31:17 +0900

    IO monad

---
+++ src/main/scala/ch13/Player.scala
@@ -0,0 +1,85 @@
+package ch13
+
+/*
+ * Copyright 2017 Yuki Toyoda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+case class Player(name: String, score: Int)
+
+trait NotPurePlayer {
+  def contest(p1: Player, p2: Player): Unit =
+    if (p1.score > p2.score) {
+      println(s"${p1.name} is the winner!")
+    } else {
+      println("It's a draw.")
+    }
+}
+
+trait PurePlayer {
+  def winner(p1: Player, p2: Player): Option[Player] =
+    if (p1.score > p2.score) Some(p1)
+    else if (p1.score < p2.score) Some(p2)
+    else None
+
+  def winnerMsg(p: Option[Player]): String =
+    p map {
+      case Player(name, _) => s"$name is the winner!"
+    } getOrElse "It's a draw."
+
+  def contest(p1: Player, p2: Player): Unit = winner(p1, p2) match {
+    case Some(Player(name, _)) => println(s"$name is the winner!")
+    case None                  => println("It's a draw.")
+  }
+
+  def contest2(p1: Player, p2: Player): Unit =
+    println(winnerMsg(winner(p1, p2)))
+}
+
+trait PurePlayerWiIO {
+
+  trait IO { self =>
+    def run: Unit
+
+    def ++(io: IO): IO = new IO {
+      override def run: Unit = {
+        self.run
+        io.run
+      }
+    }
+  }
+
+  object IO {
+    def empty: IO = new IO {
+      override def run: Unit = ()
+    }
+  }
+
+  def printLine(msg: String): IO = new IO {
+    override def run: Unit = println(msg)
+  }
+
+  def winner(p1: Player, p2: Player): Option[Player] =
+    if (p1.score > p2.score) Some(p1)
+    else if (p1.score < p2.score) Some(p2)
+    else None
+
+  def winnerMsg(p: Option[Player]): String =
+    p map {
+      case Player(name, _) => s"$name is the winner!"
+    } getOrElse "It's a draw."
+
+  def contest(p1: Player, p2: Player): IO =
+    printLine(winnerMsg(winner(p1, p2)))
+}
